8f71631ea3bd1a9a8471b9179a1ed7972f1f87f2
Маша <makrosensei@gmail.com>
2020-08-25 23:10:47 +0300

    Подправила меню

--- source/js/open-close-menu.js
+++ source/js/open-close-menu.js
@@ -3,6 +3,7 @@
 (function () {
   var navigationButton = document.querySelector('.navigation-button');
   var navigationButtonOpen = document.querySelector('.navigation-button--menu-open');
+
   var navigation = document.querySelector('.page-header__navigation');
   var footer = document.querySelector('.page-footer');
   var header = document.querySelector('.page-header__wrapper');
@@ -10,14 +11,14 @@
   navigationButton.addEventListener('click', function () {
     if (!navigationButton.classList.contains('navigation-button--menu-open')) {
       navigationButton.classList.add('navigation-button--menu-open');
-      navigation.style = 'display: flex;';
-      footer.style = 'display: block;';
-      header.style = 'background: linear-gradient(170.74deg, #633C0F -68.98%, #24150E 75.12%, #1A140D 132.97%)';
+      navigation.classList.add('page-header__navigation--menu-open');
+      footer.classList.add('page-footer--menu-open');
+      header.classList.add('page-header__wrapper--menu-open');
     } else {
       navigationButton.classList.remove('navigation-button--menu-open');
-      navigation.style = 'display: none;';
-      footer.style = 'display: none;';
-      header.style = 'background: none';
+      navigation.classList.remove('page-header__navigation--menu-open');
+      footer.classList.remove('page-footer--menu-open');
+      header.classList.remove('page-header__wrapper--menu-open');
     }
   });
 })();
