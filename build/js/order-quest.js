3b228cf674599d9be775f7b75d2e37a2b73e0bc2
Маша <makrosensei@gmail.com>
2020-09-08 00:16:29 +0300

    Ну без косяков никак

--- build/js/order-quest.js
+++ build/js/order-quest.js
@@ -1,40 +1,94 @@
 'use strict';
 
 (function () {
+  var months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+
   var sessionTimeList = document.querySelector('.sessions-time-list');
+  var sessionStatesList = document.querySelector('.sessions-states');
   var modalTypeDate = document.querySelector('.modal-type-date');
-  // modal-type-date--open
+  var dateInput = document.querySelector('.modal-type-date__input');
+  var selectedDate = document.querySelector('.selected-date');
+
+  var praceList = document.querySelectorAll('.session-time-list__prace');
+  Number(praceList);
   var selectDateButton = document.querySelector('.button-select-date');
   var questOrderWrapper = document.querySelector('.quest-order__wrapper');
+
   var dateSpan = document.querySelector('.quest-order__your-order--date-span');
   var timeSpan = document.querySelector('.quest-order__your-order--time-span');
+  var praceSpan = document.querySelector('.quest-order__your-order--prace-span');
   var sessionTimeInputs = document.querySelectorAll('.sessions-time-list__input');
 
-  var orderNavigation = document.querySelector('.page-header__navigation');
-  var orderFooter = document.querySelector('.page-footer');
-  var orderHeaderWrapper = document.querySelector('.page-header__wrapper');
+  var body = document.querySelector('.body');
+  var pageHeader = document.querySelector('.page-header__wrapper');
+  var pageContent = document.querySelector('.page-content');
+  var pageFooter = document.querySelector('.page-footer');
 
-  selectDateButton.addEventListener('click', function () {
-    if (!modalTypeDate.classList.contains('modal-type-date--open')) {
-      modalTypeDate.classList.add('modal-type-date--open');
-      orderNavigation.classList.add('page-header__navigation--menu-open');
-      orderFooter.classList.add('page-footer--menu-open');
-      headerWrapper.classList.add('page-header__wrapper--menu-open');
-    } else {
-      modalTypeDate.classList.remove('modal-type-date--open');
-      orderNavigation.classList.remove('page-header__navigation--menu-open');
-      orderFooter.classList.remove('page-footer--menu-open');
-      headerWrapper.classList.remove('page-header__wrapper--menu-open');
-    }
-  });
-
-
-  sessionTimeList.addEventListener('change', function () {
-    sessionTimeInputs.forEach(function (input) {
-      if (input.checked === true) {
+  var checkSessionAvailability = function () {
+    for (var i = 0; i < sessionTimeInputs.length; i++) {
+      if (sessionTimeInputs[i].checked === true) {
         questOrderWrapper.style = 'display: flex';
-        timeSpan.innerHTML = input.value;
+        timeSpan.innerHTML = sessionTimeInputs[i].value;
+        dateSpan.innerHTML = selectedDate.innerHTML;
+        praceSpan.innerHTML = praceList[i].innerHTML;
       }
-    });
-  });
+    }
+  };
+
+  var openPopup = function () {
+    modalTypeDate.classList.add('modal-type-date--open');
+    body.classList.add('body--date-popup-open');
+    pageHeader.classList.add('page-header__wrapper--date-popup-open');
+    pageContent.classList.add('page-content--date-popup-open');
+    pageFooter.classList.add('page-footer--date-popup-open');
+  };
+
+  var closePopup = function () {
+    modalTypeDate.classList.remove('modal-type-date--open');
+    body.classList.remove('body--date-popup-open');
+    pageHeader.classList.remove('page-header__wrapper--date-popup-open');
+    pageContent.classList.remove('page-content--date-popup-open');
+    pageFooter.classList.remove('page-footer--date-popup-open');
+
+    sessionTimeList.style = 'display: flex;';
+    sessionStatesList.style = 'display: flex;';
+
+    var newDate = new Date(dateInput.value);
+    var day = newDate.getDate();
+    var month = newDate.getMonth();
+    selectedDate.innerHTML = day + ' ' + months[month];
+
+    checkSessionAvailability();
+  };
+
+  var onButtonMouseDown = function () {
+    if (!modalTypeDate.classList.contains('modal-type-date--open')) {
+      openPopup();
+    }
+  };
+
+  var onButtonEnterPress = function (evt) {
+    if (evt.key === 'Enter' && !modalTypeDate.classList.contains('modal-type-date--open')) {
+      openPopup();
+    }
+  };
+
+  var onPopupEscPress = function (evt) {
+    if (evt.key === 'Escape' && modalTypeDate.classList.contains('modal-type-date--open')) {
+      closePopup();
+    }
+  };
+
+  var onOverlayClick = function (evt) {
+    if (!modalTypeDate.contains(evt.target) && !selectDateButton.contains(evt.target)) {
+      closePopup();
+    }
+  };
+
+  selectDateButton.addEventListener('click', onButtonMouseDown);
+  selectDateButton.addEventListener('keydown', onButtonEnterPress);
+  window.addEventListener('keydown', onPopupEscPress);
+  window.addEventListener('click', onOverlayClick);
+
+  sessionTimeList.addEventListener('change', checkSessionAvailability);
 })();
